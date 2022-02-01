402793442b6b83d6a7433f551f44181356917e31
mdherrera-2001 <mdherrera3@miners.utep.edu>
2022-02-01 09:22:00 -0700

    updating project added arrow method and changed font, still need to rotate it

--- draw_chars.c
+++ draw_chars.c
@@ -13,3 +13,30 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+void print_char_8x12(char c)
+{
+  //calculate how the shape is orientated
+  //solve what is truly moving the orientation
+  //possibly for row and column
+  c -= 0x20;
+  for(char row = 0;row < 10; row++) {
+    unsigned short rowBits = font_8x12[c][row];
+    for (char col = 0; col < 12; col++) {
+      unsigned short colMask = 1 << (11-col);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+  putchar('\n');
+  }
+}
+void print_char_5x7(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 5; row++){
+    unsigned short rowBits = font_5x7[c][row];
+    for (char col = 0; col < 7; col++) {
+      unsigned short colMask = 1 << (8-col);
+      putchar( ( rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
