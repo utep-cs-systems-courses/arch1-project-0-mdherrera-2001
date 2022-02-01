51fa184566e50172049b3a6818f776ce2a8b1e40
mdherrera-2001 <mdherrera3@miners.utep.edu>
2022-02-01 09:45:02 -0700

    corrected the orientation for the print out of 5x7 font

--- draw_chars.c
+++ draw_chars.c
@@ -31,10 +31,12 @@ void print_char_8x12(char c)
 void print_char_5x7(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 5; row++){
-    unsigned short rowBits = font_5x7[c][row];
-    for (char col = 0; col < 7; col++) {
-      unsigned short colMask = 1 << (8-col);
+  for (char col = 9; col > 0; col--){
+    unsigned short colMask = 1 << (8-col);
+    // unsigned short rowBits = font_5x7[c][row];
+    for (char row = 0; row < 5; row++) {
+      unsigned short rowBits = font_5x7[c][row];
+      // unsigned short colMask = 1 << (8-col);
       putchar( ( rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
